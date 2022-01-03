5b86091b4b5bcf9a78f774ea90cf99d43496cd99
coder9a <rajsinghbalhara@gmail.com>
2022-01-03 21:39:23 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
-  c = 'John';
   render() {
     return (
       <div>
-          hello my first class based component {this.c}
+        <NavBar/>
+        <News/>
       </div>
     )
   }
